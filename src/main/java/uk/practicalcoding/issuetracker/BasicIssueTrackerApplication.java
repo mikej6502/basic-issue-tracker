74852753eda2eb15115045ad00e73006c1c2a13b
mikej <mike@mikejenkins.co.uk>
2021-05-30 21:14:15 +0100

    Initial import

---
+++ src/main/java/uk/practicalcoding/issuetracker/BasicIssueTrackerApplication.java
@@ -0,0 +1,12 @@
+package uk.practicalcoding.issuetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicIssueTrackerApplication
+{
+	public static void main(String[] args) {
+		SpringApplication.run(BasicIssueTrackerApplication.class, args);
+	}
+}
