2e2e22491efed30a0cb995d19d4d43b4595502e2
mikej <mike@mikejenkins.co.uk>
2021-06-03 21:58:03 +0100

    Added basic Health check endpoint

---
+++ src/main/java/uk/practicalcoding/issuetracker/controller/HealthController.java
@@ -0,0 +1,16 @@
+package uk.practicalcoding.issuetracker.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController
+{
+    @GetMapping("/healthcheck")
+    public ResponseEntity<String> healthCheck()
+    {
+        return new ResponseEntity<>("OK", HttpStatus.OK);
+    }
+}
