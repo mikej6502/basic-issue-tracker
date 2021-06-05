075d064ae1e41a67901c0e5cb2f94cdcbde7c102
mikej <mike@mikejenkins.co.uk>
2021-06-05 22:42:14 +0100

    Updated CORS for Azure deployment test

--- src/main/java/uk/practicalcoding/issuetracker/BasicIssueTrackerApplication.java
+++ src/main/java/uk/practicalcoding/issuetracker/BasicIssueTrackerApplication.java
@@ -2,11 +2,28 @@ package uk.practicalcoding.issuetracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BasicIssueTrackerApplication
 {
-	public static void main(String[] args) {
-		SpringApplication.run(BasicIssueTrackerApplication.class, args);
-	}
+    public static void main( String[] args )
+    {
+        SpringApplication.run( BasicIssueTrackerApplication.class, args );
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer()
+    {
+        return new WebMvcConfigurer()
+        {
+            @Override
+            public void addCorsMappings( CorsRegistry registry )
+            {
+                registry.addMapping( "/**" ).allowedOrigins( "https://bassic-issue-tracker-ui-staging.azurewebsites.net" );
+            }
+        };
+    }
 }
